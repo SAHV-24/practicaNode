8eabc9a20a6fb6062685346ad9642e788de06325
sahv <sergio_and.herrera@uao.edu.co>
2024-02-25 11:39:54 -0500

    tailwind css config #1

--- index.js
+++ index.js
@@ -1,39 +1,44 @@
 //Declaro funciones:
-const funciones = require("./scripts/auxfunctions")
+const funciones = require("./scripts/auxfunctions");
 
-const express = require("express")
-var app = express()
-const port=8097
+const express = require("express");
+var app = express();
+const port = 8097;
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
 
 //css:
-app.use(express.static('public'))
+app.use(express.static("src"));
 
-app.get('/calculaIMC/style.css', (req, res) => {
-    res.sendFile(__dirname+"/static/style.css");
-  }) 
+app.get("/calculaIMC/style.css", (req, res) => {
+  res.sendFile(__dirname + "/src/input.css");
+});
 
-app.get("/calculaIMC",(req,res)=>{
-    res.sendFile(__dirname+"/static/index.html")
-})
+app.get("/calculaIMC", (req, res) => {
+  res.sendFile(__dirname + "/src/index.html");
+});
 
-app.post("/calculaIMC",(req,res)=>{
-    const transaccion = req.body
-    
-    const nombre = transaccion.nombre;
-    const prestamo = parseFloat(transaccion.prestamo);
-    const interes = parseFloat(transaccion.interes);
-    const meses = parseInt(transaccion.meses);
+app.post("/calculaIMC", (req, res) => {
+  const transaccion = req.body;
 
-    if (transaccion.nombre!=NaN && transaccion.prestamo != NaN && transaccion.interes!=NaN && transaccion.meses!= NaN){
+  const nombre = transaccion.nombre;
+  const prestamo = parseFloat(transaccion.prestamo);
+  const interes = parseFloat(transaccion.interes);
+  const meses = parseInt(transaccion.meses);
 
-    let cuota = funciones.calcularCuotaMensual(prestamo,interes,meses)
-    res.send(funciones.subirHTML(nombre,prestamo,interes,meses,cuota))}
+  if (
+    transaccion.nombre != NaN &&
+    transaccion.prestamo != NaN &&
+    transaccion.interes != NaN &&
+    transaccion.meses != NaN
+  ) {
+    let cuota = funciones.calcularCuotaMensual(prestamo, interes, meses);
+    res.send(funciones.subirHTML(nombre, prestamo, interes, meses, cuota));
+  }
+});
 
-})
-
-
-app.listen(port,()=>{
-    console.log("El programa se está ejecutando por: http://localhost:8097/calculaIMC");
-})
+app.listen(port, () => {
+  console.log(
+    "El programa se está ejecutando por: http://localhost:8097/calculaIMC"
+  );
+});
--- scripts/auxfunctions.js
+++ scripts/auxfunctions.js
@@ -1,57 +1,113 @@
-
-
-module.exports={
+module.exports = {
   calcularCuotaMensual: function (prestamo, interesMensual, meses) {
-
     interesMensual = interesMensual / 100;
-  
+
     let aux = Math.pow(1 + interesMensual, meses);
     let cuota = prestamo * ((aux * interesMensual) / (aux - 1));
     return Math.round(cuota);
   },
-  
-  subirHTML: function (nombre,prestamo,meses,interes,cuota){
-    return ` <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Calculadora de Cuota Mensual</title>
-    <script type="module" src="./scripts/main.js"></script>
-    <link rel="stylesheet" href="./style.css" >
-  </head>
-  <body>
-    <div>
-      <form method = "post" id="main" action="/calculaIMC" enctype="application/x-www-form-urlencoded">
-        <div>
-          <h1>Nombre:</h1>
-          <input id="nombre" placeholder="Nombre"  name="nombre"/>
-        </div>
-        <div>
-          <h1>Préstamo:</h1>
-          <input id="prestamo" placeholder="$"name="prestamo" />
-        </div>
-        <div>
-          <h1>Meses:</h1>
-          <input id="meses" name="meses" placeholder="5 meses"/>
-        </div>
-        <div>
-          <h1>Interés:</h1>
-          <input id="interes" placeholder="5%" name="interes"/>
-          <br />
-          <br />
-        </div>
-        <div>
-          <textarea id="resultado" readonly cols="50">
-El cliente:${nombre}
-pidió un préstamo de: $${prestamo} pesos
-con un interés del ${interes}% durante ${meses} meses
-Cuota: $${cuota} pesos
-</textarea>
-            <br/><br/>
-        </div>
-        <button id="enviar" type="submit">Enviar!</button>
-    </form>
-    </div>
-  </body>
-  </html`
-  }
-}
+
+  subirHTML: function (nombre, prestamo, meses, interes, cuota) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Calculadora de Cuota Mensual</title>
+        <script type="module" src="./scripts/main.js"></script>
+        <link rel="stylesheet" href="./output.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body class="bg-gradient-to-tr from-sky-600 to-[#23304c]">
+        <form
+          method="post"
+          id="main"
+          action="/calculaIMC"
+          enctype="application/x-www-form-urlencoded"
+          class="flex flex-col"
+        >
+          <div class="flex flex-col text-center round p-3 font-[Poppins]">
+            <div>
+              <h1
+                class="text-4xl font-bold bg-gradient-to-r py-2 from-sky-600 to-slate-400 bg-clip-text text-transparent"
+              >
+                Calculadora de Cuota Mensual
+              </h1>
+            </div>
+            <div class="">
+              <h1 class="text-white py-2 font-bold">Nombre</h1>
+              <input
+                id="nombre"
+                class="rounded-lg shadow-inner shadow-slate-900 h-8 placeholder:italic placeholder:text-center place text-center"
+                placeholder="Sergio"
+                name="nombre"
+                autocomplete="off"
+              />
+            </div>
+            <div>
+              <h1 class="text-white py-2 font-bold">Préstamo</h1>
+              <input
+                id="prestamo"
+                placeholder="$"
+                name="prestamo"
+                class="rounded-lg shadow-inner shadow-slate-900 h-8 placeholder:italic placeholder:text-center text-center"
+                autocomplete="off"
+              />
+            </div>
+            <div>
+              <h1 class="text-white py-2 font-bold">Meses</h1>
+              <input
+                id="meses"
+                name="meses"
+                placeholder="5 meses"
+                class="rounded-lg shadow-inner shadow-slate-900 h-8 placeholder:italic placeholder:text-center text-center"
+                autocomplete="off"
+              />
+            </div>
+            <div>
+              <h1 class="text-white py-2 font-bold">Interés</h1>
+              <input
+                id="interes"
+                placeholder="5%"
+                name="interes"
+                class="rounded-lg shadow-inner shadow-slate-900 h-8 placeholder:italic placeholder:text-center text-center"
+                autocomplete="off"
+              />
+            </div>
+    
+            <div class="py-5">
+              <div class="pb-2">
+                <button
+                  class="rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50 text-white h-10 w-40 hover:scale-110 hover:duration-300 font-semibold"
+                  id="enviar"
+                  type="submit"
+                >
+                  Enviar!
+                </button>
+              </div>
+              <textarea
+                id="resultado"
+                readonly
+                class="rounded-lg shadow-inner shadow-slate-900 w-[500px] mt-3 h-[200px]"
+              >
+
+              El cliente: ${nombre} 
+              pidió un préstamo de: $${prestamo} pesos
+              con un interés del ${interes}% durante ${meses} meses. 
+              Su cuota: $${cuota} pesos
+              
+              </textarea>
+            </div>
+          </div>
+        </form>
+      </body>
+    </html>
+    
+    `;
+  },
+};
